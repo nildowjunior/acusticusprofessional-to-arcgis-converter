a2751368fa9b377bf8035c79c1baf47e1c2c85a6
Nildo Wilpert Júnior <nildowjuniorprojetos@gmail.com>
2019-05-11 13:45:32 -0300

    use instructions

--- src/App.js
+++ src/App.js
@@ -45,24 +45,37 @@ export default class App extends React.Component {
         return (
             <div className="container">
                 <div className="row">
+                    {"Instruções de uso: "}
+                    <ol>
+                        <li>{"Acesse o site "} <a target="_blank" rel="noopener noreferrer"
+                                                  href="http://acusticusprofessional.telegrafia.eu/Acusticus.aspx">{"acusticus professional"}</a>
+                        </li>
+                        <li>{"Abra as ferramentas de desenvolvedor (pressione F12 ou Ctrl + Shift + i)"}</li>
+                        <li>{"Nas ferramentas de desenvolvedor acesse a aba \"Network\""}</li>
+                        <li>{"Pressione F5 para recarregar a página"}</li>
+                        <li>{"Utilizar a opção de filtro da parte superior para buscar o request \"loadProjectLayerWithVisibleGeoObjects\""}</li>
+                        <li>{"Seleciona o item apresentado"}</li>
+                        <li>{"Acesse a aba \"preview\" e copie o conteúdo da resposta (apenas a parte textual sem copiar o \" do inicio e do final"}</li>
+                        <li>{"Cole o conteúdo copiado no espaço abaixo e pressione para realizar a conversão"}</li>
+                    </ol>
                     <p>
-                        {"Para converter os arquivos CSVs gerados para o formato KMZ você pode utilizar o site "}
-                        <a target="_blank" rel="noopener noreferrer"
-                           href="https://mygeodata.cloud/converter/latlong-to-kmz">
-                            {"deste link"}
-                        </a>
+                        {"Para converter os arquivos CSVs gerados para o formato KMZ você pode utilizar o site  "} <a
+                        target="_blank" rel="noopener noreferrer"
+                        href="https://mygeodata.cloud/converter/latlong-to-kmz">
+                        {"deste link"}
+                    </a>
                     </p>
                 </div>
                 <div className="row">
                     <div
                         className="col-md-5"
                     >
-                  <textarea name="data" id="data" style={{
-                      width: "100%",
-                      minHeight: "500px",
-                      boxSizing: "border-box"
-                  }} ref={this.inputTextArea}
-                  />
+                            <textarea name="data" id="data" style={{
+                                width: "100%",
+                                minHeight: "500px",
+                                boxSizing: "border-box"
+                            }} ref={this.inputTextArea}
+                            />
                     </div>
                     <div className="col-md-2">
                         <button onClick={this.handleOnClickGenerateFiles}>Generate csv files</button>
